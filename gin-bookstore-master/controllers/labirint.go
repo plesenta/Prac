fc08c11d7e99a61cc2fc73ef7eff1d63e2cc3bc9
plesenta <isip_n.i.vlasova@mpt.ru>
2022-01-18 20:10:07 +0300

    update

--- gin-bookstore-master/controllers/labirint.go
+++ gin-bookstore-master/controllers/labirint.go
@@ -28,6 +28,7 @@ var Pattern = [20][20]int{{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
 var x = 0
 var y = 0
+var pos Coordinates
 
 type Cmd struct {
 	Command string `json:"command"`
@@ -40,9 +41,9 @@ type Coordinates struct {
 }
 
 func Movement(c *gin.Context) {
+
 	var input Cmd
-	//var pos Coordinates
-	pos := Coordinates{0, 0, 0, 0}
+
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -51,7 +52,6 @@ func Movement(c *gin.Context) {
 	if input.Command == "/move_top" {
 		if Pattern[x][y-1] == 1 {
 			y -= 1
-			pos.Top += 1
 			c.JSON(http.StatusOK, gin.H{"message": "Команда успешно выполнена!"})
 		} else if Pattern[x][y-1] <= 0 {
 			c.JSON(http.StatusNotFound, gin.H{"message": "Прохода нет!"})
@@ -62,7 +62,6 @@ func Movement(c *gin.Context) {
 	} else if input.Command == "/move_bot" {
 		if Pattern[x][y+1] == 1 {
 			y += 1
-			pos.Bot -= 1
 			c.JSON(http.StatusOK, gin.H{"message": "Команда успешно выполнена!"})
 		} else if Pattern[x][y+1] <= 0 {
 			c.JSON(http.StatusNotFound, gin.H{"message": "Прохода нет!"})
@@ -72,7 +71,6 @@ func Movement(c *gin.Context) {
 	} else if input.Command == "/move_left" {
 		if Pattern[x-1][y] == 1 {
 			x -= 1
-			pos.Left -= 1
 			c.JSON(http.StatusOK, gin.H{"message": "Команда успешно выполнена!"})
 		} else if Pattern[x+1][y] <= 0 {
 			c.JSON(http.StatusNotFound, gin.H{"message": "Прохода нет!"})
@@ -82,7 +80,6 @@ func Movement(c *gin.Context) {
 	} else if input.Command == "/move_right" {
 		if Pattern[x+1][y] == 1 {
 			x += 1
-			pos.Right += 1
 			c.JSON(http.StatusOK, gin.H{"message": "Команда успешно выполнена!"})
 		} else if Pattern[x-1][y] <= 0 {
 			c.JSON(http.StatusNotFound, gin.H{"message": "Прохода нет!"})
@@ -90,7 +87,30 @@ func Movement(c *gin.Context) {
 			c.JSON(http.StatusOK, gin.H{"message": "Вы нашли выход!"})
 		}
 	} else if input.Command == "/check" {
+
+		if Pattern[x][y-1] == 1 {
+			pos.Top = 1
+		} else {
+			pos.Top = 0
+		}
+		if Pattern[x][y+1] == 1 {
+			pos.Bot = 1
+		} else {
+			pos.Bot = 0
+		}
+		if Pattern[x+1][y] == 1 {
+			pos.Right = 1
+		} else {
+			pos.Right = 0
+		}
+		if Pattern[x-1][y] == 1 {
+			pos.Left = 1
+		} else {
+			pos.Left = 0
+		}
+
 		c.IndentedJSON(http.StatusOK, pos)
+
 	} else {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "400"})
 	}
